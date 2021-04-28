b18204f2613176b714a81c90b688ef1974131b59
Milleth <milleth.manzanilla@gsfe.tupcavite.edu.ph>
2021-04-28 21:40:08 +0800

    Done with ORM yahuu

--- BMIList/tests.py
+++ BMIList/tests.py
@@ -1,49 +1,54 @@
 #from django.urls import resolve
 from BMIList.models import Item
 from django.test import TestCase
-#from BMIList.views import BMIPage
+from BMIList.views import BmiPage
 #from django.http import HttpRequest 
 class BmiPageTest(TestCase):
+	def test_uses_template(self):
+		response = self.client.get ('/')
+		self.assertTemplateUsed(response,'bmi.html')
+	
+	def test_save_only_if_necessary(self):
+		response = self.client.get ('/')
+		self.assertEqual(Item.objects.count(), 0)
+	
+	def test_at_request(self):
+		response = self.client.post('/', data={'age':'Age'})
+#		self.assertIn('age', response.content.decode())
+#		self.assertTemplateUsed(response,'bmi.html')
+		
+		self.assertEqual(Item.objects.count(), 1)
+		newItem = Item.objects.first()
+		self.assertEqual(newItem.text, 'Age')
+		
+	def test_palit_POST(self):
+		response = self.client.post('/', data={'age':'age'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+		
+	def test_template_display_items(self):
+	        Item.objects.create(text= 'mgm1')
+	        Item.objects.create(text= 'mgm2')
+	        response = self.client.get('/')
+	        self.assertIn('mgm1', response.content.decode())
+	        self.assertIn('mgm2', response.content.decode())
 
-    def test_bmipage_returns_correct_view(self):
-        response = self.client.get ('/')
-        self.assertTemplateUsed(response,'bmi.html')
-        
-    def test_save_post_request(self):
-        response = self.client.post('/', data={'age':'Age'})
-        self.assertIn('Age', response.content.decode())
-        self.assertTemplateUsed(response,'bmi.html')
+
+
 class ORMTest(TestCase):
 
-    def test_saveretrievelist(self):
-    	txtItem1=Item()
-    	txtItem1.text='item one'
-    	txtItem1.save()
-    	txtItem2=Item()
-    	txtItem2.text='item two'
-    	txtItem2.save()
-    	saved_Items=Item.objects.all()
-    	self.assertEqual(saved_Items.count(),2)
-    	savedItem1=saved_Items[0]
-    	savedItem2=saved_Items[1]
-    	self.assertEqual(savedItem1.text, 'item one')
-    	self.assertEqual(savedItem2.text, 'item two')
-        
-    '''def test_uses_mainpage_template(self):
-        response = self.client.get ('/')
-        self.assertTemplateUsed(response,'bmi.html')'''
-        
-    
-         
-    '''def test_bmipage_returns_correct_view(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        string_html = render_to_string('bmi.html')
-        self.assertEqual(html,string_html)
-        self.assertTemplateUsed(response,'bmi.html')'''
+	def test_saveretrievelist(self):
+		txtItem1=Item()
+		txtItem1.text='item one'
+		txtItem1.save()
+		txtItem2=Item()
+		txtItem2.text='item two'
+		txtItem2.save()
+		saved_Items=Item.objects.all()
+		self.assertEqual(saved_Items.count(),2)
+		savedItem1=saved_Items[0]
+		savedItem2=saved_Items[1]
+		self.assertEqual(savedItem1.text, 'item one')
+		self.assertEqual(savedItem2.text, 'item two')
 
-
-  #def test_root_url_resolve_to_bmipage_view(self):
-   #found = resolve('/')
-   #self.assertEqual(found.func, BmiPage)
 
--- BMIList/views.py
+++ BMIList/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from BMIList.models import Item
 
@@ -6,7 +6,12 @@ from BMIList.models import Item
 
 #BmiPage = None
 def BmiPage(request):
-#    return HttpResponse('<html><head><title>"BMICalculator"</title></head><body><h1>Body Mass Index Calculator</h1><form><input id="height" name="height" type="text" placeholder="what is your height (in inches)?"> <br><br><input id="weight" name="weight" type="text" placeholder="what is your weight (in kg)?"> <br><input type="submit" id="enter" name="enter" value="enter"></form><p>Your BMIis: <span id="output">?</span></p><p>This means you are: value = "output" </p> </body></html>')
-    #if request.method == 'POST':
-    	#return HttpResponse(request.POST['name'])
-    return render (request, "bmi.html", {'Age':request.POST.get('age'), 'Height':request.POST.get('height'), 'Weight':request.POST.get('weight'''),}) 
+  if request.method =='POST':
+#  	newItem = request.POST['age']
+  	Item.objects.create(text=request.POST['age'])
+  	return redirect('/')
+  items = Item.objects.all()
+  #else:
+#  	newItem=''
+  	
+  return render (request, 'bmi.html', {'Age':items})
