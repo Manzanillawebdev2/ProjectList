61b13acb6ce6a8d63b7f5447f7f1606b9c821690
Milleth <milleth.manzanilla@gsfe.tupcavite.edu.ph>
2021-04-29 14:56:41 +0800

    ag commit muna ako, live server, pass

--- Mgm/tests.py
+++ Mgm/tests.py
@@ -7,24 +7,32 @@ import unittest
 
 from selenium.webdriver.common.keys import Keys
 import time
+from django.test import LiveServerTestCase
+waitkita = 3
 
 
-
-
-class PageTest(unittest.TestCase):
+class PageTest(LiveServerTestCase):
 
 	def setUp(self):
 	   self.browser = webdriver.Firefox()
 	   
 	#def tearDown(self):
 	   #self.browser.quit()
-	def check_rows_in_list_table(self, row_text):
-	   table = self.browser.find_element_by_id('tableko')
-	   rows = table.find_elements_by_tag_name('tr')
-	   self.assertIn(row_text, [row.text for row in rows]) 
+	def wait_rows_in_list_table(self, row_text):
+	   start_time =time.time()
+	   while time.time()-start_time < waitkita:
+	   	time.sleep (.1)
+	   try:
+	   	table = self.browser.find_element_by_id('tableko')
+	   	rows = table.find_elements_by_tag_name('tr')
+	   	self.assertIn(row_text, [row.text for row in rows])
+	   	return
+	   except(AssertionError.WebDriverException) as e:
+	   	if time.time()-start_time > waitkita:
+	   		raise e
 	   
 	def test_start_list_and_retrieve_it(self):
-           self.browser.get('http://localhost:8000')
+           self.browser.get(self.live_server_url)
            self.assertIn('"BMICalculator"', self.browser.title)
            headerText = self.browser.find_element_by_tag_name('h1').text
            self.assertIn('Body Mass Index Calculator', headerText)
@@ -41,18 +49,18 @@ class PageTest(unittest.TestCase):
            self.assertEqual(inputheight1.get_attribute('placeholder'),'what is your height (in cm)?')
            self.assertEqual(inputweight1.get_attribute('placeholder'),'what is your weight (in kg)?')
     
-           time.sleep(1)
+           time.sleep(.1)
            inputage1.send_keys('21')
-           time.sleep(1)
+           time.sleep(.1)
            inputgender1.send_keys('female')
-           time.sleep(1)
+           time.sleep(.1)
            inputheight1.send_keys('156')
-           time.sleep(1)
+           time.sleep(.1)
            inputweight1.send_keys('42')
-           time.sleep(1)
+           time.sleep(.1)
            btnenter.click()
-           time.sleep(1)
-           self.check_rows_in_list_table('1: 21')
+           time.sleep(.1)
+           self.wait_rows_in_list_table('1: 21')
            
 #This page should update and show the name of the list
            inputage1 = self.browser.find_element_by_id('age')
@@ -64,25 +72,25 @@ class PageTest(unittest.TestCase):
            self.assertEqual(inputgender1.get_attribute('placeholder'),'male or female')
            self.assertEqual(inputheight1.get_attribute('placeholder'),'what is your height (in cm)?')
            self.assertEqual(inputweight1.get_attribute('placeholder'),'what is your weight (in kg)?')
-           time.sleep(1)
+           time.sleep(.1)
     
-           time.sleep(1)
+           time.sleep(.1)
            inputage1.send_keys('18')
-           time.sleep(1)
+           time.sleep(.1)
            inputgender1.send_keys('male')
-           time.sleep(1)
+           time.sleep(.1)
            inputheight1.send_keys('155')
-           time.sleep(1)
+           time.sleep(.1)
            inputweight1.send_keys('50')
-           time.sleep(1)
+           time.sleep(.1)
            btnenter.click()
-           time.sleep(1)
-           self.check_rows_in_list_table('2: 18')
+           time.sleep(.1)
+           self.wait_rows_in_list_table('2: 18')
 '''           table = self.browser.find_element_by_id('tableko')
            rows = table.find_elements_by_tag_name('tr')
            self.assertIn('21 156 42',[row.text for row in rows])
 	   #self.fail('Finish the test NOW!!!????')'''
 	   
 	   
-if __name__== '__main__':
-   unittest.main(warnings='ignore')
+#if __name__== '__main__':
+#   unittest.main(warnings='ignore')
